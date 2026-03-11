814fa3484d3ee01f5f1bc351452a435e76a3092b
Hussein Mazyad <MazyadH1@s2.rpn.ch>
2026-03-11 20:54:10 +0100

    Updates

--- _next/static/Er07IrByUySQKOTCYWxL6/_buildManifest.js
+++ _next/static/Er07IrByUySQKOTCYWxL6/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/agency//_next/:path+",
+        "destination": "/agency/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
